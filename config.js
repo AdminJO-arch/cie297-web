931934a8d5c8043ba7d16f51759f0db1c2319d6c
AdminJO-arch <josueisrraeltininisthenchs@gmail.com>
2025-10-11 12:53:04 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Cambia esta URL por la del backend en Render, por ejemplo:
+// window.CIE297_API = "https://tu-backend.onrender.com";
+// Config para producción
+window.CIE297_API = "https://cie297-backend-1.onrender.com";
